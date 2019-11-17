45f63e123c574e2e646446d706d577884ecf55a7
Bryce Haley <bryce_haley@sfu.ca>
2019-11-17 10:52:44 -0800

    can export LED pins

--- src/led.c
+++ src/led.c
@@ -4,16 +4,16 @@
 #include <time.h>
 #include <unistd.h>
 
-#define YELLOW = 0
-#define RED = 1
-#define BLUE = 2
-#define GPIO_DIR = "/sys/class/gpio"
-#define NUM_LIGHTS = 3
-#define BUFFER_SIZE = 256
+#define YELLOW 0
+#define RED 1
+#define BLUE 2
+#define GPIO_DIR "/sys/class/gpio/"
+#define NUM_LIGHTS 3
+#define BUFFER_SIZE 256
 
 static const char *GPIO[] = {"26", "46", "47"};
 
-void LED_init()
+void LED_init(void)
 {
 	FILE *file;
 	char buffer[BUFFER_SIZE] = "";
@@ -32,7 +32,7 @@ void LED_init()
 
 	nanosleep((const struct timespec[]){{0, 300000000L}}, NULL);//sleep here ensures files get updated before access
 	//Set directions as in
-	for(int i = 0; i < LIGHTS; ++i) {
+	for(int i = 0; i < NUM_LIGHTS; ++i) {
 		strcpy(buffer, GPIO_DIR "gpio");
 		strcat(buffer, GPIO[i]);
 		strcat(buffer, "/direction");
