bb70965ff1794aa883b610b8ad213f1de6b8bf40
Bryce Haley <bryce_haley@sfu.ca>
2019-11-16 23:45:17 -0800

    init commit

---
+++ include/led.h
@@ -0,0 +1,15 @@
+#ifndef LED_H
+#define LED_H
+
+void LED_red_flash(void);
+void LED_red_on(void);
+void LED_red_off(void);
+
+void LED_blu_flash(void);
+void LED_blu_on(void);
+void LED_blu_off(void);
+
+void LED_yel_flash(void);
+void LED_yel_on(void);
+void LED_yel_off(void);
+#endif
